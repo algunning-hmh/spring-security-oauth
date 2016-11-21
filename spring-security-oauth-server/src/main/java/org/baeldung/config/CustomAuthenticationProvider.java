ea9e70c10661e920ee3bf8ca1119855fb52262a9
algunning-hmh <alan.gunning@hmhco.com>
2016-11-21 09:52:22 +0000

    commit notes

--- spring-security-oauth-server/src/main/java/org/baeldung/config/CustomAuthenticationProvider.java
+++ spring-security-oauth-server/src/main/java/org/baeldung/config/CustomAuthenticationProvider.java
@@ -23,6 +23,35 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
        // } else {
        //     return null;
        // }
+
+
+        /*
+        * Option 1: Add a new CompositeTokenGranter
+        * Option 2: Subclass UsernamePasswordAuthenticationToken and return it here.
+        *
+        * */
+//http://www.programcreek.com/java-api-examples/index.php?api=org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+
+    /*
+    * THink i need to set something up in DefaultToken services
+    *
+        public class DefaultTokenServices
+        extends Object
+        implements AuthorizationServerTokenServices, ResourceServerTokenServices, ConsumerTokenServices, InitializingBean
+
+        Base implementation for token services using random UUID values for the access token and refresh token values. The main extension point for customizations is the TokenEnhancer which will be called after the access and refresh tokens have been generated but before they are stored.
+        Persistence is delegated to a TokenStore implementation and customization of the access token to a TokenEnhancer.
+
+        Friday leaving note:
+        Looks like tokenEnhancer does everything i need
+
+        Left to do Monday:
+        POC out the /authorise code flow to see if openid conenct will work for use
+        using TokenEnhancer etc...
+
+
+    * */
+
     }
 
     @Override
